33decedb4cddf9b2d490e4c76af11c68ff4f799e
Louis <sonyxfvr46@outlook.de>
2024-12-04 22:12:18 +0100

    implementiere modus 2: erstelle ein eigene Meldung csv

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,16 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <windows.h>
 
 #define DELIMITER ',' // Delimiter als Makro
+#define MELDUNG_HEADER "Name,Vorname,Geburtsjahr,Graduierung,Verein,Land,Kategorie,Geschlecht,Kommentar"
+#define TEILNEHMERLISTE_HEADER "Name,Vorname,Geburtsjahr,Graduierung,Verein,Land,Kategorie,ID,file_ID,Geschlecht,Kommentar"
+
+#define C_NORM enum_ccolor::WHITE
+#define C_HEAD enum_ccolor::CYAN
+#define C_OPT enum_ccolor::RED
+#define C_USER enum_ccolor::BRIGHT_WHITE
 
 namespace fs = std::filesystem;
 
@@ -23,6 +31,44 @@ struct Teilnehmer {
     char geschlecht;
     std::string kommentar;
 };
+
+
+enum class enum_ccolor {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    YELLOW = 6,
+    WHITE = 7,
+    GRAY = 8,
+    LIGHT_BLUE = 9,
+    LIGHT_GREEN = 10,
+    LIGHT_CYAN = 11,
+    LIGHT_RED = 12,
+    LIGHT_MAGENTA = 13,
+    LIGHT_YELLOW = 14,
+    BRIGHT_WHITE = 15
+};
+
+// Funktion, um die Konsolenfarbe zu setzen
+void SetConsoleColor(enum_ccolor color = C_NORM) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, static_cast<int>(color));
+}
+
+
+void printUser() {
+
+    SetConsoleColor(enum_ccolor::GREEN);
+    std::cout << "\nuser@generator"; 
+    SetConsoleColor(enum_ccolor::WHITE); 
+    std::cout << ":";
+    SetConsoleColor(enum_ccolor::LIGHT_MAGENTA); 
+    std::cout << "~ $ ";
+    SetConsoleColor(enum_ccolor::WHITE);
+}
 
 // Funktion zum Parsen einer Zeile
 Teilnehmer parseLine(const std::string& line) {
@@ -90,7 +136,7 @@ void writeCSVFile(const std::string& filename, const std::vector<std::pair<Teiln
     }
 
     // Header schreiben
-    file << "Name,Vorname,Geburtsjahr,Graduierung,Verein,Land,Kategorie,ID,file_ID,Geschlecht,Kommentar\n";
+    file << TEILNEHMERLISTE_HEADER << "\n";
 
     // Daten schreiben
     int id = 1; // ID-Zaehler beginnt bei 1
@@ -113,15 +159,170 @@ void writeCSVFile(const std::string& filename, const std::vector<std::pair<Teiln
     file.close();
 }
 
-int main() {
-    std::cout << "Willkommen zum JudoShiaiMeldungenGenerator!\n\n";
-    std::cout << "Bitte waehlen Sie eine Option:\n";
-    std::cout << "1. Vorhandene Meldungen*.csv-Dateien aus dem Verzeichnis auslesen und zusammenfassen.\n";
-    std::cout << "2. Eine neue Meldungen*.csv-Datei hinzufuegen.\n";
-    std::cout << "\nIhre Wahl (1 oder 2): ";
+void createNewCSVFile() {
 
+    std::cout << "Wie soll der Name der neuen Datei nach 'Meldungen_' lauten?";
+    std::string fileSuffix;
+
+    printUser();
+    SetConsoleColor(C_USER);
+    std::cin >> fileSuffix;
+    SetConsoleColor();
+
+    std::string filename = "Meldungen_" + fileSuffix + ".csv";
+
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Fehler beim Erstellen der Datei: " << filename << "\n";
+        return;
+    }
+
+    // Header in die Datei schreiben
+    file << MELDUNG_HEADER << "\n";
+    std::cout << "\nDatei '" << filename << "' wurde angelegt. Erstellen wir Datensaetze!\n";
+
+
+    while (true) {
+        Teilnehmer t;
+
+        // Benutzereingaben für jeden Datenpunkt
+        std::cout << "\nGeben Sie die Daten zum Judoka ein:\n";
+
+        std::cout << "Name\t\t(Bauer)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin.ignore();
+        std::getline(std::cin, t.name);
+        SetConsoleColor();
+
+        std::cout << "Vorname\t\t(Anna)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::getline(std::cin, t.vorname);
+        SetConsoleColor();
+
+        std::cout << "Geburtsjahr\t(2000)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin >> t.geburtsjahr;
+        SetConsoleColor();
+
+        std::cout << "Guertel\t\t(1k/2d)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin >> t.graduierung;
+        SetConsoleColor();
+
+        std::cout << "Verein\t\t(DJK Berlin)\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin.ignore();
+        std::getline(std::cin, t.verein);
+        SetConsoleColor();
+
+        std::cout << "Land\t\t(GER)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin >> t.land;
+        SetConsoleColor();
+
+        std::cout << "Kategorie\t(U18w-57)\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin >> t.gewicht;
+        SetConsoleColor();
+
+        std::cout << "Geschlecht\t(m / w)\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin >> t.geschlecht;
+        SetConsoleColor();
+
+        std::cout << "Kommentar\t\t\t: ";
+
+        SetConsoleColor(C_USER);
+        std::cin.ignore();
+        std::getline(std::cin, t.kommentar);
+        SetConsoleColor();
+
+        // Datensatz anzeigen
+        std::cout << "\nDer eingegebene Datensatz lautet:\n";
+
+        SetConsoleColor(C_USER);
+        std::cout 
+            << t.name << DELIMITER
+            << t.vorname << DELIMITER
+            << t.geburtsjahr << DELIMITER
+            << t.graduierung << DELIMITER
+            << t.verein << DELIMITER
+            << t.land << DELIMITER
+            << t.gewicht << DELIMITER
+            << t.geschlecht << DELIMITER
+            << t.kommentar << "\n";
+        SetConsoleColor();
+
+        // Benutzer entscheiden lassen
+        std::cout << "\nWollen Sie diesen Datensatz speichern?\n";
+
+        SetConsoleColor(C_OPT);
+        std::cout << " -'j'\t= Datensatz speichern und naechsten eingeben\n";
+        std::cout << " -'n'\t= Datensatz verwerfen\n";
+        std::cout << " -'end'\t= Datensatz speichern und Programm beenden\n";
+
+        SetConsoleColor();
+
+        printUser();
+        SetConsoleColor(C_USER);
+        std::string choice;
+        std::cin >> choice;
+        SetConsoleColor();
+
+        if (choice == "n" || choice == "N") {
+            std::cout << "Datensatz wurde verworfen.\n";
+            continue; // Nächsten Datensatz eingeben
+        }
+
+        // Datensatz speichern
+        file << t.name << DELIMITER
+            << t.vorname << DELIMITER
+            << t.geburtsjahr << DELIMITER
+            << t.graduierung << DELIMITER
+            << t.verein << DELIMITER
+            << t.land << DELIMITER
+            << t.gewicht << DELIMITER
+            << t.geschlecht << DELIMITER
+            << t.kommentar << "\n";
+        std::cout << "Datensatz gespeichert.\n";
+
+        if (choice == "end" || choice == "END") {
+            std::cout << "Programm wird beendet.\n";
+            break; // Programm beenden
+        }
+
+    }
+
+    file.close();
+    std::cout << "\nDie Datei '" << filename << "' wurde erfolgreich gespeichert.\n";
+}
+
+int main() {
+
+    SetConsoleColor(C_HEAD);
+    std::cout << "Willkommen zum JudoShia Meldungen Generator\n\n";
+    SetConsoleColor();
+
+    std::cout << "Was wollen Sie tun?\n";
+    SetConsoleColor(C_OPT);
+    std::cout << " -'1' Vorhandene Meldungen*.csv-Dateien aus dem Verzeichnis auslesen und zusammenfassen.\n";
+    std::cout << " -'2' Eine neue Meldungen*.csv-Datei hinzufuegen.";
+    SetConsoleColor();
+
+
+    printUser();
+    SetConsoleColor(C_USER);
     int userChoice;
     std::cin >> userChoice;
+    SetConsoleColor();
 
     switch (userChoice) {
     case 1: {
@@ -167,19 +368,12 @@ int main() {
         std::cout << "Gesamtanzahl der Datensaetze in der Teilnehmerliste: " << gesamtDatensaetze << "\n";
         std::cout << "Ergebnisse wurden in " << outputFilename << " geschrieben.\n";
 
-        // Benutzerinteraktion vor Beendigung
-        std::cout << "\n\nDruecken Sie die Eingabetaste, um das Programm zu beenden..." << std::endl;
-        std::cin.get(); // Warten auf Eingabe von Enter
+        
 
         break;
     }
     case 2: {
-        std::cin.ignore(); // Eingabe-Puffer bereinigen nach std::cin >> userChoice
-
-        std::cout << "\nDie Funktion zum Hinzufuegen neuer Dateien ist noch nicht implementiert.\n";
-        std::cout << "Druecken Sie die Eingabetaste, um das Programm zu beenden..." << std::endl;
-        std::cin.get(); // Warten auf Eingabe von Enter
-
+        createNewCSVFile(); // Neue Datei erstellen und Datensätze hinzufügen
         break;
     }
     default: {
@@ -187,6 +381,10 @@ int main() {
         break;
     }
     }
+
+    // Benutzerinteraktion vor Beendigung
+    std::cout << "\n\nDruecken Sie die Eingabetaste, um das Programm zu beenden..." << std::endl;
+    std::cin.get(); // Warten auf Eingabe von Enter
 
     return 0;
 }
